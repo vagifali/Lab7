d23f71487d62c35f7a851eb3ca6add5056b56bbd
Вагиф <71507953+vagifali@users.noreply.github.com>
2020-12-19 19:59:38 +0300

    Add files via upload

--- Main.java
+++ Main.java
@@ -1,34 +1,60 @@
-package ru.mirea.gib04.lab8;
-
-import java.io.*;
+package ru.mirea.gib04.lab7;
+import java.util.LinkedList;
+import java.util.ArrayList;
 
 public class Main {
-    public static void main(String[] args){
-        try{
-            FileWriter file = new FileWriter("C:/Users/Vagif/IdeaProjects/Aliev/src/ru/mirea/gib04/lab8/note.txt",false);
-            String text = "Мама мыла раму, раму мыла мама";
-            file.write(text);
-            file.write("Какой-то текст");
-            file.append('\n');
-            file.append('E');
-            file.flush();
+    public static void main(String[] args) {
+
+        ArrayList<String> vaze = new ArrayList<String>();
+        vaze.add("яблоко");
+        vaze.add("груша");
+        for(String t : vaze) System.out.println(t);
 
 
-        } catch(IOException ex){
-            System.out.println(ex.getMessage());
+        LinkedList<String> states = new LinkedList<String>();
+        states.add("Германия");
+        states.add("Россия");
+        states.add("Польша");
+        states.addFirst("Великобритания");
+        states.addLast("Вьетнам");
+        states.add(1,"Китай");
+
+        /* Процедурный подход */
+        for( int i=0; i<states.size();i++)
+            System.out.println(states.get(i));
+
+        states.remove(1);
+
+        System.out.println("/////////////\n");
+
+        /* Объектно-ориентированный подход */
+        for(String p : states){
+            System.out.println(p);
+
         }
 
-        try {
-            FileReader reader = new FileReader("C:/Users/Vagif/IdeaProjects/Aliev/src/ru/mirea/gib04/lab8/note.txt");
-            int c;
-            while ((c = reader.read()) != -1) {
-                System.out.println((char)c);
-            }
-        } catch(IOException ex) {
-            System.out.println(ex.getMessage());
+        if(states.contains("Вьетнам")) System.out.println("Есть такое государство");
+        states.remove(2);
+        states.remove("Польша");
+
+        for(String p : states) {
+            System.out.println(p);
         }
-        
+
+        LinkedList<Person> friends = new LinkedList<Person>();
+        friends.add(new Person("Stepan"));
+        friends.add(1, new Person ("Mike"));
+        friends.addFirst(new Person("Tom"));
+        friends.addLast(new Person("Nick"));
+
+        for(Person p : friends) System.out.println(p);
+        friends.remove(1);
+        for(Person p : friends) System.out.println(p);
+        Person tmp = friends.getFirst();
+        System.out.println(tmp);
 
 
     }
+
+
 }
